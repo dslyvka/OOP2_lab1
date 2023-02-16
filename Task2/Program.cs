df17f7bee7378320a3031649c787cc958c313779
dslyvka <daniilslyvka@gmail.com>
2023-02-16 20:06:10 +0200

    isDone

---
+++ Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        delegate int Delegate();
+        static void Main(string[] args)
+        {
+            Delegate[] delegates = new Delegate[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                delegates[i] = delegate
+                {
+                    Random rnd = new Random();
+                    return rnd.Next(1, 100);
+                };
+            }
+
+            Func<Delegate[], double> average = delegate (Delegate[] arr)
+            {
+                double sum = 0;
+                foreach (Delegate del in arr)
+                {
+                    sum += del();
+                }
+                return sum / arr.Length;
+            };
+
+            Console.WriteLine("The average value is: " + average(delegates));
+
+            Console.ReadLine();
+        }
+    }
+}
